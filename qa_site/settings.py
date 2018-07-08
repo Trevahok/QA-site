3b712ace6a1cba1c8958e136b0f09e1c5da43ecf
Rajagopalan <g.raju2000@gmail.com>
2018-07-08 13:06:36 +0530

    Updated login screen

--- qa_site/settings.py
+++ qa_site/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'qa_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qa',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -114,7 +118,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+print(PROJECT_DIR)
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
