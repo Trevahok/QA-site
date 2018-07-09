ddbdff66d0963f8bfc49aa046aafd8f39c0f545e
Trevahok <vighneshss@gmail.com>
2018-07-09 22:27:00 +0530

    make userprofile model, add last ip and last seen middleware

--- qa_site/settings.py
+++ qa_site/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'qa_site.registration.middleware.lastseen',
 ]
 
 ROOT_URLCONF = 'qa_site.urls'
--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,21 @@
+from django.core.validators import RegexValidator
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class UserProfile(models.Model):
+    ''' A model for user profile page that stores all the specifics. '''
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Others')
+    )
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be a valid number. ")
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    email_id = models.EmailField()
+    ph_no = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    reg_date = models.DateField(blank = False)
+    last_login = models.DateTimeField()
+    last_activity_ip = models.GenericIPAddressField()
+
