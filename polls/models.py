8668b10791a6235e348e74c9084f3830e862a22d
Trevahok <vighneshss@gmail.com>
2018-07-17 18:18:43 +0530

    remove pics

--- polls/models.py
+++ polls/models.py
@@ -24,10 +24,14 @@ class Faculty(models.Model):
             (4, 'four'),
             (5, 'five'),
             )
-    pic = models.ImageField()
+    pic = models.ImageField(upload_to='photos',blank=True)
     name = models.CharField(default = '', max_length=50)
     post = models.CharField(default = '' , max_length=50)     # postiton like sr.professor or associate prof. etc.
     dept = models.CharField(default = '' , max_length=7, choices=dept_choices)
     likes = models.PositiveSmallIntegerField(default =0 )
     dislikes = models.PositiveSmallIntegerField(default=0)
     rating = models.PositiveSmallIntegerField(default = 0 , choices= star_choices)
+
+
+    def __str__(self):
+        return self.name
--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from .models import Faculty
 from django.http import HttpResponse
 # Create your views here.
 def test(request):
-    return HttpResponse('<p>here u are, {{request.user.username}} </p>')
+    return render(request,'faculty.html') 
