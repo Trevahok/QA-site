81d5f65c971ab01ee0533148ed1c582fd94e56c1
Rajagopalan <g.raju2000@gmail.com>
2018-07-24 20:08:21 +0530

    Added availability of details variable throughout the site

--- qa_site/context_processors.py
+++ qa_site/context_processors.py
@@ -1,3 +1,8 @@
 from polls.models import dept_choices_list
+from registration.models import UserProfile
 def faculties_var(request):
     return {'schools':dept_choices_list}
+def user_var(request):
+    if request.user.is_authenticated:
+        instance = UserProfile.objects.get(user= request.user)
+    return {'details':instance}
--- qa_site/views.py
+++ qa_site/views.py
@@ -4,6 +4,5 @@ from registration.models import UserProfile
 from polls.models import dept_choices_list
 def home(request):
     if request.user.is_authenticated:
-        instance = UserProfile.objects.get(user= request.user)
-        return(render(request,'home.html',{'details':instance}))
+        return(render(request,'home.html'))
     return(render(request,'home.html'))
